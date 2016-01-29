6db73758f02febb532106be35a1673569f639f77
Jesse Gumm <gumm@sigma-star.com>
2016-01-29 16:16:32 -0600

    Fix bugs in btn

--- src/element_btn.erl
+++ src/element_btn.erl
@@ -10,11 +10,11 @@
 reflect() -> record_info(fields, btn).
 
 transform_element(Btn = #btn{glyphicon=Icon, type=Type, size=Size}) ->
-	Button = wf_utils:copy_fields(Btn, #button{}),
 	BtnTypeClass = coldstrap_util:type_class(btn, Type),
 	SizeClass = coldstrap_util:type_class(btn, Size),
-	CurrClass = Btn#button.class,
-	Body = add_glyph_body(Icon, Button#button.body),
+	CurrClass = Btn#btn.class,
+	Body = add_glyph_body(Icon, Btn#btn.body),
+	Button = wf_utils:copy_fields(Btn, #button{}),
 	Button#button{
 	 	body=Body,
 		class=[btn, CurrClass,SizeClass,BtnTypeClass]
