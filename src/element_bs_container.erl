0da340037882433f4296825d5e8e88c0f03b8a19
Jesse Gumm <gumm@sigma-star.com>
2017-01-09 15:33:33 -0600

    Fix typo in bs_container

--- src/element_bs_container.erl
+++ src/element_bs_container.erl
@@ -11,4 +11,4 @@ reflect() -> record_info(fields,bs_container).
 
 transform_element(Rec = #bs_container{}) ->
 	Panel = wf_utils:copy_fields(Rec, #panel{}),
-    Panel#panel{class=[Panel#bs_container.class, 'container']}.
+    Panel#panel{class=[Rec#bs_container.class, 'container']}.
